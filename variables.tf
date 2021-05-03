6cebdb4eee8a1bd7b6a21ed0b351a6a4b477e162
j.dhirendra.panchal <janakpanchal13@gmail.com>
2021-05-03 21:55:09 +0530

    added new version

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "aws_region" {
+  default = "us-west-2"
+}
+
+variable "access_key" {
+   default = "AKIASAQHOYXB25FVJT5L"
+}
+
+variable "secret_key" {
+  default = "mR1nw8kBaGKcBtUaezk0hXVLUtprhs/h8B/1X3MF"
+}
+
+variable "cluster-name" {
+  default = "terraform-eks-demo"
+  type    = string
+}
